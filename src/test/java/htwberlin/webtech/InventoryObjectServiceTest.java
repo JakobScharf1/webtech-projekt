d77075ee606dff73547282bfd34aa12148406136
Maximilian Vorwerk <maximilian.vorwerk@student.htw-berlin.de>
2023-07-02 17:28:38 +0200

    Bereinigung der Import-Anweisungen (Keine Fachlichen bzw. Logischen Anpassungen)

--- src/test/java/htwberlin/webtech/InventoryObjectServiceTest.java
+++ src/test/java/htwberlin/webtech/InventoryObjectServiceTest.java
@@ -6,22 +6,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
 
-import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 public class InventoryObjectServiceTest {

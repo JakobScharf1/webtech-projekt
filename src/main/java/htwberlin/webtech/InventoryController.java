b716710ff6ef450b6e4d265807549be84aa58c7a
your_username <your_email@gmail.com>
2023-07-03 14:45:14 +0200

    Bugfix - update PUT

--- src/main/java/htwberlin/webtech/InventoryController.java
+++ src/main/java/htwberlin/webtech/InventoryController.java
@@ -28,8 +28,8 @@ public class InventoryController {
         return service.save(inventoryObject);
     }
 
-    @PutMapping("/inventoryObject/{id}")
-    public void updateInventoryObject(@PathVariable String id, @PathVariable String name, @PathVariable int amount) {
+    @PutMapping("/inventoryObject/{id}/{name}/{amount}")
+    public void updateInventoryObject(@PathVariable("id") String id, @PathVariable("name") String name, @PathVariable("amount") int amount) {
         Long inventoryObjectId = Long.parseLong(id);
         service.update(inventoryObjectId, name, amount);
     }
